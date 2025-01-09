52e6c6b33d94d1eaefa33c70026b8c2f764602e3
Serg Litov <Serg@litov.com>
2025-01-09 09:54:55 +0300

    seminar2:Dz3 complete

--- src/seminar2/Dz3.java
+++ src/seminar2/Dz3.java
@@ -5,7 +5,17 @@ import java.util.Arrays;
 class StringSorter {
     public static String[] sortStrings(String[] strings) {
         // Введите свое решение ниже
-
+        if (strings == null) {
+            System.out.println("strings == null");
+            return new String[0];
+        }
+        try {
+        Arrays.sort(strings);
+        return strings;
+        } catch (Exception e) {
+            System.out.println("Exception");
+            return new String[0];
+        }
     }
 }
 
